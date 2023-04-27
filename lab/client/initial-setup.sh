878a48b52a3cdabf834f1476b419c85128d1c856
Wendell Borges <perlporter@gmail.com>
2023-04-27 10:17:00 -0300

    instalando o winbox no cliente

--- lab/client/initial-setup.sh
+++ lab/client/initial-setup.sh
@@ -11,13 +11,16 @@ apt install -y ubuntu-desktop-minimal \
     lightdm \
     net-tools \
     traceroute \
-    firefox
+    firefox 
 
 dpkg --configure -a
 
 # remove pacotes desnecessários (incluindo tela inicial de "boas-vindas")
 apt remove -y gnome-initial-setup
 apt autoremove -y
+
+# instalando o winbox
+snap install winbox
 
 # configurando o lightdm pq o gdm3 não tava funcionando
 echo "/usr/sbin/lightdm" > /etc/X11/default-display-manager
